f4d8efacafe082e4cda19546c7460d0379ea25fd
Hiltlairen <sirjhoswilliam5@gmail.com>
2023-10-19 11:52:39 -0400

    profile realizado

--- src/components/top.jsx
+++ src/components/top.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {text} from '../assets/text'
+import './styles/general.css'
 function Top() {
     const imgProfile=text[0].imgProfile;
     const imgFondo=text[0].imgFondo;
     return ( 
         <>
-            <div>
+            <div className="top">
                 <img src={imgFondo} />
                 <img src={imgProfile} />
             </div>
